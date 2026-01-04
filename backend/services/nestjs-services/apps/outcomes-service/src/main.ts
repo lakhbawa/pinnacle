6141fb37b67cc130dc3d3ece975afd652e57587f
Lakh Bawa <lakhveer.bawa@viamo.io>
2026-01-04 08:41:24 -0500

    Hot + Refactor: keep case of fields

--- backend/services/nestjs-services/apps/outcomes-service/src/main.ts
+++ backend/services/nestjs-services/apps/outcomes-service/src/main.ts
@@ -1,24 +1,28 @@
 import {NestFactory} from '@nestjs/core';
 import {MicroserviceOptions, Transport} from "@nestjs/microservices";
-import { OutcomesServiceModule } from './outcomes-service.module';
-import { join } from 'path';
+import {OutcomesServiceModule} from './outcomes-service.module';
+import {join} from 'path';
 import {outcomes} from "@app/common";
 
 async function bootstrap() {
 
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-      OutcomesServiceModule,
-      {
-        transport: Transport.GRPC,
-        options: {
-            url: '0.0.0.0:4440',
-          protoPath: join(__dirname, "../outcomes.proto"),
-          package: outcomes.OUTCOMES_V1_PACKAGE_NAME
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        OutcomesServiceModule,
+        {
+            transport: Transport.GRPC,
+            options: {
+                url: '0.0.0.0:4440',
+                protoPath: join(__dirname, "../outcomes.proto"),
+                package: outcomes.OUTCOMES_V1_PACKAGE_NAME,
+                loader: {
+                    keepCase: true,
+                },
+            }
         }
-      }
-  )
+    )
 
-  await app.listen() // default port is 5000
+    await app.listen() // default port is 5000
 }
+
 bootstrap();
