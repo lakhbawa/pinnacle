a3a10f4812028859165c41c6bab7ffa5d37d9508
Lakh Bawa <lakhveer.bawa@viamo.io>
2026-01-06 17:42:17 -0500

    Revert + Temporary: removed kafka code from focuscontroller

--- backend/services/nestjs-services/apps/outcomes-service/src/focus/focus.controller.ts
+++ backend/services/nestjs-services/apps/outcomes-service/src/focus/focus.controller.ts
@@ -1,28 +1,18 @@
-import {Controller, Inject} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { FocusService } from './focus.service';
 import {ActiveFocusDataRequest, ActiveFocusDataResponse,
   FocusServiceController,
   FocusServiceControllerMethods
 } from "@app/common/types/outcomes_service/v1/focus";
 import {OutcomeMapper} from "../mappers/outcome.mapper";
-import {ClientKafka} from "@nestjs/microservices";
 
 @Controller()
 @FocusServiceControllerMethods()
 export class FocusController implements FocusServiceController {
-  constructor(private readonly focusService: FocusService, @Inject('KAFKA_SERVICE') private kafka: ClientKafka) {
-  }
-  async onModuleInit() {
-    await this.kafka.connect();
+  constructor(private readonly focusService: FocusService) {
   }
 
   async getActiveFocusData(request: ActiveFocusDataRequest): Promise<ActiveFocusDataResponse> {
-
-    this.kafka.emit('active_focus.request', {
-      message: "First Message",
-      timestamp: new Date().toISOString(),
-    });
-
       console.log("Reached method");
         const outcomes =  await this.focusService.getActiveFocusData(request);
         return {
