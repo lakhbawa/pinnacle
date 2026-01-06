e7caf657be5c26f7f48c33d4a87ee151522dd823
Lakh Bawa <lakhveer.bawa@viamo.io>
2026-01-06 15:14:00 -0500

    minor improvements to prisma service

--- backend/services/nestjs-services/apps/auth-service/src/prisma.service.ts
+++ backend/services/nestjs-services/apps/auth-service/src/prisma.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
 import { PrismaClient } from './generated/prisma-client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
         const pool = new Pool({
             connectionString: process.env.DATABASE_URL,
@@ -16,4 +16,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         await this.$connect();
     }
+    async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
